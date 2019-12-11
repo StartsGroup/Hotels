a0f51335a6aa11dc13b2a16b363ee3b796d3c145
lihanwen <445804560@qq.com>
2019-12-11 20:03:32 +0800

    用户,团队,员工模块2019-11 20:03

---
+++ provider01/src/main/java/com/hotel/controller/TeamController.java
@@ -0,0 +1,48 @@
+package com.hotel.controller;
+
+
+import com.hotel.pojo.Team;
+import com.hotel.service.TeamService;
+import org.springframework.web.bind.annotation.*;
+
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/team")
+public class TeamController {
+    @Resource
+    private TeamService teamService;
+
+    @GetMapping("/all")
+    public List<Team> getAllTeam(){
+        return teamService.getAllTeam();
+    }
+    @PostMapping("/login")
+    public Team login(@RequestBody Team t){
+        return teamService.login(t);
+    }
+    @PostMapping("ve")
+    public String saveTeam(@RequestBody Team t){
+        boolean flag=teamService.saveTeam(t);
+        String result=(flag ? "success":"fail");
+        return result;
+    }
+    @GetMapping("/all/{tid}")
+    public Team getTeamByTid(@PathVariable int tid){
+        return teamService.getTeamByTid(tid);
+    }
+    @GetMapping("/delete/{tid}")
+    public String deleteTeamByTid(@PathVariable int tid){
+        boolean flag=teamService.deleteTeamByTid(tid);
+        String result=(flag ? "success":"fail");
+        return result;
+    }
+    @PostMapping("/update")
+    public String updateTeamByTid(@RequestBody Team t){
+        boolean flag=teamService.updateTeamByTid(t);
+        String result=(flag ? "success":"fail");
+        return result;
+    }
+}
---
+++ provider01/src/main/java/com/hotel/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.hotel.controller;
+
+import com.hotel.pojo.Users;
+import com.hotel.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Resource
+    private UserService userService;
+
+    @GetMapping("/all")
+    public List<Users> getAllUsers(){
+        return userService.getAllUsers();
+    }
+    @PostMapping("/login")
+    public Users login(@RequestBody Users u){
+        return userService.login(u);
+    }
+    @PostMapping("ve")
+    public String saveUsers(@RequestBody Users u){
+        boolean flag=userService.saveUsers(u);
+        String result=(flag ? "success":"fail");
+        return result;
+    }
+    @PostMapping("/delete/{uid}")
+    public String deleteUsersByUid(@PathVariable int uid){
+        boolean flag=userService.deleteUserByUid(uid);
+        String result=(flag ? "success":"fail");
+        return result;
+    }
+    @GetMapping("/all/{uid}")
+    public Users getUsersByUid(@PathVariable int uid){
+        return userService.getUsersByUid(uid);
+    }
+    @PostMapping("/update")
+    public String updateUsersByUid(Users u){
+        boolean flag=userService.updateUsersByUid(u);
+        String result=(flag ? "success":"fail");
+        return result;
+    }
+}
